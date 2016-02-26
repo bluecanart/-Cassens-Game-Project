b7874ad05db2e1317f2c7e0ae17a58689366589e
bluecanart <bluecanart@me.com>
2016-02-26 14:01:16 -0700

    More map changes

--- MapGeneration.java
+++ MapGeneration.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication1;
+package MapGeneration;
 
 import java.util.*;
 
@@ -10,49 +10,26 @@ import java.util.*;
  *
  * @author cc228719
  */
-public class JavaApplication1 {
+public class MapGeneration {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        generateMap(20, 8);
-    }
-
-    static void generateMap(int maxRooms, int size) {
-
-        String map[][] = new String[size][size];
-        Random myRand = new Random();
-        map[myRand.nextInt(size)][myRand.nextInt(size)] = "o ";
-
-        for (int i = 0; i < maxRooms; i++) {
-            if (i < maxRooms - 1) {
-                
-                ArrayList<Integer[]> slots = findAvaliable(map, size);
-                int nextSlot = myRand.nextInt(slots.size());
-                map[slots.get(nextSlot)[0]][slots.get(nextSlot)[1]] = "+ ";
-                
-            } else {
-                
-                ArrayList<Integer[]> slots = findAvaliableBoss(map, size);
-                int nextSlot = myRand.nextInt(slots.size());
-                map[slots.get(nextSlot)[0]][slots.get(nextSlot)[1]] = "x ";
-                
-            }
-        }
+    	int size = 8;
+    	int maxRooms = 20;
+    	
+        String map[][] = generateMap(maxRooms, size);
+        map = populateRooms(map, size);
 
         for (int i = 0; i < size; i++) {
 
             for (int s = 0; s < size; s++) {
                 if (map[i][s] == null) {
-                    System.out.print("\u001B[37m"+"- "+"\u001B[0m");
-                } else if (map[i][s] == "x ") {
-                    System.out.print("\u001B[31m"+map[i][s]+"\u001B[0m");
-                } else if (map[i][s] == "o ") {
-                    System.out.print("\u001B[34m"+map[i][s]+"\u001B[0m");
+                    System.out.print("-----");
                 } else {
-                    System.out.print("\u001B[32m"+map[i][s]+"\u001B[0m");
+                    System.out.print(map[i][s]);
                 }
 
             }
@@ -60,7 +37,57 @@ public class JavaApplication1 {
             System.out.println("");
 
         }
+    }
 
+    static String[][] generateMap(int maxRooms, int size) {
+
+        String map[][] = new String[size][size];
+        Random myRand = new Random();
+        map[myRand.nextInt(size)][myRand.nextInt(size)] = "o";
+
+        for (int i = 0; i < maxRooms; i++) {
+            if (i < maxRooms - 1) {
+                
+                ArrayList<Integer[]> slots = findAvaliable(map, size);
+                int nextSlot = myRand.nextInt(slots.size());
+                map[slots.get(nextSlot)[0]][slots.get(nextSlot)[1]] = "+";
+                
+            } else {
+                
+                ArrayList<Integer[]> slots = findAvaliableBoss(map, size);
+                int nextSlot = myRand.nextInt(slots.size());
+                map[slots.get(nextSlot)[0]][slots.get(nextSlot)[1]] = "x";
+                
+            }
+        }
+        
+        return map;
+
+    }
+    
+    static String[][] generateMap(int maxRooms, int size, long seed) {
+
+        String map[][] = new String[size][size];
+        Random myRand = new Random(seed);
+        map[myRand.nextInt(size)][myRand.nextInt(size)] = "o";
+
+        for (int i = 0; i < maxRooms; i++) {
+            if (i < maxRooms - 1) {
+                
+                ArrayList<Integer[]> slots = findAvaliable(map, size);
+                int nextSlot = myRand.nextInt(slots.size());
+                map[slots.get(nextSlot)[0]][slots.get(nextSlot)[1]] = "+";
+                
+            } else {
+                
+                ArrayList<Integer[]> slots = findAvaliableBoss(map, size);
+                int nextSlot = myRand.nextInt(slots.size());
+                map[slots.get(nextSlot)[0]][slots.get(nextSlot)[1]] = "x";
+                
+            }
+        }
+        
+        return map;
 
     }
 
@@ -140,6 +167,63 @@ public class JavaApplication1 {
 
         }
         return avaliableSlots;
+
+    }
+    
+    static String[][] populateRooms(String[][] map, int size) {
+
+        for (int i = 0; i < size; i++) {
+
+            for (int s = 0; s < size; s++) {
+                	
+                if (map[i][s] != null){
+                	
+                	int top = 0;
+                    int left = 0;
+                    int right = 0;
+                    int bottom = 0;
+
+                    if (i > 0) {
+
+                        if (map[i - 1][s] != null) {
+                            top += 1;
+                        }
+
+                    }
+
+                    if (s > 0) {
+
+                        if (map[i][s - 1] != null) {
+                            left += 1;
+                        }
+
+                    }
+
+                    if (i < size - 1) {
+
+                        if (map[i + 1][s] != null) {
+                            bottom += 1;
+                        }
+
+                    }
+
+                    if (s < size - 1) {
+
+                        if (map[i][s + 1] != null) {
+                            right += 1;
+                        }
+
+                    }
+                    
+                    map[i][s] = map[i][s] + top + right + bottom + left;
+                	
+                }
+
+            }
+
+        }
+        
+        return map;
 
     }
     
